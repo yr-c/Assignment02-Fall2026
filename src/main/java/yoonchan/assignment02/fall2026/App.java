70e6724c0fa79720a7b3f5cdd650ad507d75dfd7
yoonc_feo8r6n <yoonc_feo8r6n@10.16.135.48>
2026-09-14 10:12:07 -0400

    Created calulcateReceipt method

--- src/main/java/yoonchan/assignment02/fall2026/App.java
+++ src/main/java/yoonchan/assignment02/fall2026/App.java
@@ -33,20 +33,17 @@ public class App extends Application {
         Label parkingFeesLabel = new Label("Parking Fees:");
         Label taxiChargesLabel = new Label("Taxi charges:");
         Label registrationFeesLabel = new Label("Event registration fees:");
-        Label lodgingChargesLabel = new Label("Lodging charges (per night):");
+        Label lodgingFeesLabel = new Label("Lodging fees (per night):");
         Button calculateButtonLabel = new Button("Calculate expenses");
 
         Label[] leftLabels = {numDaysLabel, airfareLabel, carRentalLabel, milesDrivenLabel,
-            parkingFeesLabel, taxiChargesLabel, registrationFeesLabel, lodgingChargesLabel};
+            parkingFeesLabel, taxiChargesLabel, registrationFeesLabel, lodgingFeesLabel};
 
         for (int i = 0; i < leftLabels.length; i++) {
             left.add(leftLabels[i], 0, i);
         }
 
         left.add(calculateButtonLabel, 0, leftLabels.length);
-
-        calculateButtonLabel.setOnAction(e -> {
-        });
 
         TextField numDaysField = new TextField();
         TextField airFareField = new TextField();
@@ -55,11 +52,11 @@ public class App extends Application {
         TextField parkingFeesField = new TextField();
         TextField taxiChargesField = new TextField();
         TextField registrationFeesField = new TextField();
-        TextField lodgingChargesField = new TextField();
+        TextField lodgingFeesField = new TextField();
 
         TextField[] leftFields = {numDaysField, airFareField, carRentalField,
             milesDrivenField, parkingFeesField, taxiChargesField,
-            registrationFeesField, lodgingChargesField};
+            registrationFeesField, lodgingFeesField};
 
         for (int i = 0; i < leftFields.length; i++) {
             left.add(leftFields[i], 1, i);
@@ -85,4 +82,47 @@ public class App extends Application {
         launch();
     }
 
+    /**
+     * Calculates all trip expenses, and returns the receipt.
+     *
+     * @param numDays The number of days of the trip
+     * @param airFare The airfare cost
+     * @param carRentalFees The car rental cost
+     * @param milesDriven The amount of miles driven on a private vehicle
+     * @param parkingFees The parking cost
+     * @param taxiFees The taxi cost
+     * @param registrationFees The seminar or conference registration fees
+     * @param lodgingFees The lodging fees
+     * @return An array of double corresponding to: Total expenses,
+     * reimbursement by company, expenses to to the client, amount saved by the
+     * client.
+     *
+     */
+    private static double[] calculateReciept(int numDays, double airFare,
+            double carRentalFees, double milesDriven, double parkingFees, double taxiFees,
+            double registrationFees, double lodgingFees) {
+
+        double totalExpenses = airFare + carRentalFees + parkingFees + taxiFees
+                + registrationFees + lodgingFees * numDays;
+
+        double paidParkingPerDay = 10d;
+        double paidTaxiFeesPerDay = 20d;
+        double paidLodgingFeesPerDay = 95d;
+        double paidPerMile = 0.27;
+
+        double allowableExpenses = 37d * numDays + numDays * paidParkingPerDay
+                + numDays * paidTaxiFeesPerDay + numDays * paidLodgingFeesPerDay
+                + milesDriven * paidPerMile;
+
+        double reimnbursement = 37d * numDays + Math.min(parkingFees, numDays * paidParkingPerDay)
+                + Math.min(taxiFees, numDays * paidTaxiFeesPerDay)
+                + Math.min(lodgingFees * numDays, numDays * paidLodgingFeesPerDay)
+                + milesDriven * paidPerMile;
+
+        double expensesDue = totalExpenses - reimnbursement;
+
+        double amountSaved = allowableExpenses - totalExpenses;
+
+        return new double[]{totalExpenses, allowableExpenses, expensesDue, amountSaved};
+    }
 }
