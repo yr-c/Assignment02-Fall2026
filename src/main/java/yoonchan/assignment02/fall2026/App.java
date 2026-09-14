7649abe518e96bbeed8d9735bfa3a9d673623d90
yoonc_feo8r6n <yoonc_feo8r6n@10.16.135.48>
2026-09-14 09:21:26 -0400

    Created initial UI for input fields

--- src/main/java/yoonchan/assignment02/fall2026/App.java
+++ src/main/java/yoonchan/assignment02/fall2026/App.java
@@ -1,11 +1,15 @@
 package yoonchan.assignment02.fall2026;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 
 /**
  * JavaFX App
@@ -14,11 +18,47 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        HBox root = new HBox();
+        GridPane left = new GridPane();
+        VBox right = new VBox();
+        root.getChildren().addAll(left, right);
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        left.setAlignment(Pos.CENTER);
+        left.setPadding(new Insets(10));
+        Label numDays = new Label("Number of trip days:");
+        Label airfare = new Label("Airfare ($):");
+        Label carRental = new Label("Car rental:");
+        Label milesDriven = new Label("Miles driven:");
+        Label parkingFees = new Label("Parking Fees:");
+        Label taxiCharges = new Label("Taxi charges:");
+        Label registrationFees = new Label("Event registration fees:");
+        Label lodgingCharges = new Label("Lodging charges (per night):");
+
+        Label[] leftLabels = {numDays, airfare, carRental, milesDriven,
+            parkingFees, taxiCharges, registrationFees, lodgingCharges};
+
+        for (int i = 0; i < leftLabels.length; i++) {
+            left.add(leftLabels[i], 0, i);
+        }
+
+        right.setAlignment(Pos.CENTER);
+        TextField numDaysField = new TextField();
+        TextField airFareField = new TextField();
+        TextField carRentalField = new TextField();
+        TextField milesDrivenField = new TextField();
+        TextField parkingFeesField = new TextField();
+        TextField taxiChargesField = new TextField();
+        TextField registrationFeesField = new TextField();
+        TextField lodgingChargesField = new TextField();
+
+        TextField[] leftFields = {numDaysField, airFareField, carRentalField,
+            milesDrivenField, parkingFeesField, taxiChargesField,
+            registrationFeesField, lodgingChargesField};
+
+        for (int i = 0; i < leftFields.length; i++) {
+            left.add(leftFields[i], 1, i);
+        }
+        Scene scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
