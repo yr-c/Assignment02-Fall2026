58a532c883d65a0de7ace7de0b15a6e5faeea737
yoonc_feo8r6n <yoonc_feo8r6n@10.16.135.48>
2026-09-14 08:43:31 -0400

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module yoonchan.assignment02.fall2026 {
+    requires javafx.controls;
+    exports yoonchan.assignment02.fall2026;
+}
---
+++ src/main/java/yoonchan/assignment02/fall2026/SystemInfo.java
@@ -0,0 +1,13 @@
+package yoonchan.assignment02.fall2026;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
